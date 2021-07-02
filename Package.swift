fc21cf5a82717faf3b16fdbf0e06a8b2b5ad7503
buildserver-v4core <uk.github.auth@2gis.ru>
2021-07-02 21:12:28 +0700

    Bump version to 1.0.0-rc.2

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
@@ -14,8 +13,8 @@ let package = Package(
     .binaryTarget(
       name: "DGis",
       url:
-        "https://maven.2gis.dev/sdk-ios-rc/sdk-release-v1.0.0/Release/map-RC-1625158954969.xcframework.zip",
-      checksum: "e109db7b1b4a05104ef037079e1d721912ce972c40b831570f726e813b8ccd2c"
+        "https://artifactory.2gis.dev/sdk-ios-rc/sdk-release-v1.0.0/Release/map-RC-1625233169253.xcframework.zip",
+      checksum: "fdfa88144807d1c886749c17d9924cd0963e802abe9f50cad69c9012c937807c"
     )
   ]
 )
