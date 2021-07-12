fba29f572b1a040d7bfb820b268b736c7f8d2b3b
buildserver-v4core <uk.github.auth@2gis.ru>
2021-07-12 13:56:42 +0700

    Bump version to lpha.2-rc.1

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     .binaryTarget(
       name: "DGis",
       url:
-        "https://artifactory.2gis.dev/sdk-ios-rc/sdk-release-v2.0.0-alpha.1/Release/map-RC-1625808849219.xcframework.zip",
-      checksum: "f6d44868aa8110472078f80e336093205f56b54b7a34faf8877634d7aa476b18"
+        "https://artifactory.2gis.dev/sdk-ios-rc/sdk-release-v2.0.0-alpha.2/Release/map-RC-1626070035397.xcframework.zip",
+      checksum: "aad3fd62cfe50e606e6f804e7ed3ce4d86802401b0f5f6f0ab0d31267d7c7543"
     )
   ]
 )
