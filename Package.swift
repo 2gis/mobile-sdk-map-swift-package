a49f06a605f9325076d00adf2620af93519262b0
buildserver-v4core <uk.github.auth@2gis.ru>
2022-01-26 16:26:23 +0700

    Bump version to 1.6.16

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     .binaryTarget(
       name: "DGis",
       url:
-        "https://artifactory.2gis.dev/sdk-ios-release/1.6.15/Release/DGisMapSDK.zip",
-      checksum: "75d20552d8824e97bcfbebdcb68fd00b3504f7c3730dbeef90fb959c6d3ac86e"
+        "https://artifactory.2gis.dev/sdk-ios-release/1.6.16/Release/DGisMapSDK.zip",
+      checksum: "cbd463e7bdf14a621edcb80f612c6151e94a4bfafb38c4220d5d8907da676ed8"
     )
   ]
 )
